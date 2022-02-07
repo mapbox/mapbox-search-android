129aa5199a9b898dc10ba47e5ad95f2e01250ee8
Dzmitry Fomchyn <5161509+DzmitryFomchyn@users.noreply.github.com>
2022-02-07 18:04:26 +0300

    Fix script path (#22)
    * Fix script path
    
    * Update reviews list

--- scripts/ci-publish-snapshots.sh
+++ scripts/ci-publish-snapshots.sh
@@ -6,7 +6,7 @@ VERSION_NAME=`cat MapboxSearch/gradle.properties | grep versionName`
 if [[ "$VERSION_NAME" == *-SNAPSHOT ]]
   then
     echo "Publishing snapshots to the SDK registry..."
-    source scripts/publish.sh
+    source scripts/ci-publish.sh
   else
     echo "Current version is not SNAPSHOT. Skip publishing."
 fi
