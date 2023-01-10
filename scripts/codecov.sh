6ffd61a4b6f026b715b2f8b3efc9c83ad04c4f4c
Dzmitry Fomchyn <5161509+DzmitryFomchyn@users.noreply.github.com>
2023-01-10 14:55:37 +0100

    Remove codecov token (#118)

--- scripts/codecov.sh
+++ scripts/codecov.sh
@@ -9,6 +9,5 @@ pip3 install --user codecov
 env -i python3 -m \
     codecov \
     --root "$(pwd)" \
-    --token ${CODECOV_TOKEN} \
     --file "$1" \
     --required || echo 'Codecov failed to upload'
