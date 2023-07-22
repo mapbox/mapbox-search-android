4dfc72de4225e7be14bd90f8021eb4fb2b329beb
dzmitryfomchyn <Dzmitry.Fomchyn@mapbox.com>
2023-07-22 10:57:07 +0200

    Category refactoring

--- MapboxSearch/ui/src/main/java/com/mapbox/search/ui/view/category/Category.kt
+++ MapboxSearch/ui/src/main/java/com/mapbox/search/ui/view/category/Category.kt
@@ -8,23 +8,22 @@ import kotlinx.parcelize.Parcelize
 
 /**
  * Represents a category of POI. This class also defines a list of commonly used categories.
- * Internal: SBS categories canonical names https://github.com/mapbox/tag-to-category/blob/master/reference/categories.csv
+ * Internal: Search Box category canonical names https://github.com/mapbox/tag-to-category/blob/master/reference/categories.csv
  */
 @Suppress("LargeClass")
 @Parcelize
 internal class Category(
 
     /**
-     * Category canonical name used in Geocoding API. Can be empty if you don't use SBS endpoint for search.
+     * Category canonical name used in Geocoding API.
      * See [List of the most common POI categories](https://docs.mapbox.com/api/search/geocoding/#point-of-interest-category-coverage).
      */
-    val geocodingCanonicalName: String,
+    val geocodingName: String,
 
     /**
-     * Category canonical name used in Single Box Search API. Can be empty if you don't use SBS endpoint for search.
-     * Contact our team if you need more information about SBS categories.
+     * Category canonical name used in Search Box API.
      */
-    val sbsCanonicalName: String,
+    val searchBoxName: String,
 
     /**
      * Category visual presentation.
@@ -41,8 +40,8 @@ internal class Category(
 
         other as Category
 
-        if (geocodingCanonicalName != other.geocodingCanonicalName) return false
-        if (sbsCanonicalName != other.sbsCanonicalName) return false
+        if (geocodingName != other.geocodingName) return false
+        if (searchBoxName != other.searchBoxName) return false
         if (presentation != other.presentation) return false
 
         return true
@@ -52,8 +51,8 @@ internal class Category(
      * @suppress
      */
     override fun hashCode(): Int {
-        var result = geocodingCanonicalName.hashCode()
-        result = 31 * result + sbsCanonicalName.hashCode()
+        var result = geocodingName.hashCode()
+        result = 31 * result + searchBoxName.hashCode()
         result = 31 * result + presentation.hashCode()
         return result
     }
@@ -63,8 +62,8 @@ internal class Category(
      */
     override fun toString(): String {
         return "Category(" +
-                "geocodingCanonicalName='$geocodingCanonicalName', " +
-                "sbsCanonicalName='$sbsCanonicalName', " +
+                "geocodingName='$geocodingName', " +
+                "searchBoxName='$searchBoxName', " +
                 "presentation=$presentation" +
                 ")"
     }
@@ -74,17 +73,17 @@ internal class Category(
      * See [MAKI icons for common points of interest](https://labs.mapbox.com/maki-icons/).
      */
     @Parcelize
-    public class Presentation(
+    class Presentation(
 
         /**
          * The resource identifier of the string resource to be displayed as category name.
          */
-        @StringRes public val displayName: Int,
+        @StringRes val displayName: Int,
 
         /**
          * The resource identifier of the drawable to be displayed as category icon.
          */
-        @DrawableRes public val icon: Int,
+        @DrawableRes val icon: Int,
     ) : Parcelable {
 
         /**
@@ -122,15 +121,15 @@ internal class Category(
     /**
      * Companion object.
      */
-    public companion object {
+    companion object {
 
         /**
          * Predefined category constant.
          */
         @JvmField
-        public val RESTAURANTS: Category = Category(
-            geocodingCanonicalName = "restaurant",
-            sbsCanonicalName = "restaurant",
+        val RESTAURANTS: Category = Category(
+            geocodingName = "restaurant",
+            searchBoxName = "restaurant",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_restaurants,
                 R.drawable.mapbox_search_sdk_ic_category_restaurant
@@ -141,9 +140,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val BARS: Category = Category(
-            geocodingCanonicalName = "bar",
-            sbsCanonicalName = "bar",
+        val BARS: Category = Category(
+            geocodingName = "bar",
+            searchBoxName = "bar",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_bars,
                 R.drawable.mapbox_search_sdk_ic_category_bar
@@ -154,9 +153,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val COFFEE_SHOP_CAFE: Category = Category(
-            geocodingCanonicalName = "cafe",
-            sbsCanonicalName = "cafe",
+        val COFFEE_SHOP_CAFE: Category = Category(
+            geocodingName = "cafe",
+            searchBoxName = "cafe",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_coffee_shop_cafe,
                 R.drawable.mapbox_search_sdk_ic_category_cafe
@@ -167,9 +166,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val HOTEL: Category = Category(
-            geocodingCanonicalName = "hotel",
-            sbsCanonicalName = "hotel",
+        val HOTEL: Category = Category(
+            geocodingName = "hotel",
+            searchBoxName = "hotel",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_hotel,
                 R.drawable.mapbox_search_sdk_ic_category_hotel
@@ -180,9 +179,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val GAS_STATION: Category = Category(
-            geocodingCanonicalName = "fuel",
-            sbsCanonicalName = "gas_station",
+        val GAS_STATION: Category = Category(
+            geocodingName = "fuel",
+            searchBoxName = "gas_station",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_gas_station,
                 R.drawable.mapbox_search_sdk_ic_category_gas
@@ -193,9 +192,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val EV_CHARGING_STATION: Category = Category(
-            geocodingCanonicalName = "charging station",
-            sbsCanonicalName = "charging_station",
+        val EV_CHARGING_STATION: Category = Category(
+            geocodingName = "charging station",
+            searchBoxName = "charging_station",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_ev_charging_station,
                 R.drawable.mapbox_search_sdk_ic_category_charging_station
@@ -206,9 +205,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val PARKING: Category = Category(
-            geocodingCanonicalName = "parking",
-            sbsCanonicalName = "parking_lot",
+        val PARKING: Category = Category(
+            geocodingName = "parking",
+            searchBoxName = "parking_lot",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_parking,
                 R.drawable.mapbox_search_sdk_ic_category_parking
@@ -219,9 +218,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val BUS_STATION: Category = Category(
-            geocodingCanonicalName = "bus station",
-            sbsCanonicalName = "bus_station",
+        val BUS_STATION: Category = Category(
+            geocodingName = "bus station",
+            searchBoxName = "bus_station",
             Presentation(
                 R.string.mapbox_search_sdk_category_name_bus_station,
                 R.drawable.mapbox_search_sdk_ic_category_bus
@@ -232,9 +231,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val RAILWAY_STATION: Category = Category(
-            geocodingCanonicalName = "train station",
-            sbsCanonicalName = "railway_station",
+        val RAILWAY_STATION: Category = Category(
+            geocodingName = "train station",
+            searchBoxName = "railway_station",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_train_station,
                 R.drawable.mapbox_search_sdk_ic_category_train
@@ -245,9 +244,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val SHOPPING_MALLS: Category = Category(
-            geocodingCanonicalName = "mall",
-            sbsCanonicalName = "shopping_mall",
+        val SHOPPING_MALLS: Category = Category(
+            geocodingName = "mall",
+            searchBoxName = "shopping_mall",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_shopping_malls,
                 R.drawable.mapbox_search_sdk_ic_category_shopping_mall
@@ -258,9 +257,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val SUPERMARKET_GROCERY: Category = Category(
-            geocodingCanonicalName = "grocery",
-            sbsCanonicalName = "grocery",
+        val SUPERMARKET_GROCERY: Category = Category(
+            geocodingName = "grocery",
+            searchBoxName = "grocery",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_supermarket_grocery,
                 R.drawable.mapbox_search_sdk_ic_category_grocery
@@ -271,9 +270,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val CLOTHING_STORE: Category = Category(
-            geocodingCanonicalName = "clothes",
-            sbsCanonicalName = "clothing_store",
+        val CLOTHING_STORE: Category = Category(
+            geocodingName = "clothes",
+            searchBoxName = "clothing_store",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_clothing_shoes,
                 R.drawable.mapbox_search_sdk_ic_category_clothing_store
@@ -284,9 +283,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val PHARMACY: Category = Category(
-            geocodingCanonicalName = "pharmacy",
-            sbsCanonicalName = "pharmacy",
+        val PHARMACY: Category = Category(
+            geocodingName = "pharmacy",
+            searchBoxName = "pharmacy",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_pharmacy,
                 R.drawable.mapbox_search_sdk_ic_category_pharmacy
@@ -297,9 +296,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val MUSEUMS: Category = Category(
-            geocodingCanonicalName = "museum",
-            sbsCanonicalName = "museum",
+        val MUSEUMS: Category = Category(
+            geocodingName = "museum",
+            searchBoxName = "museum",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_museums,
                 R.drawable.mapbox_search_sdk_ic_category_museum
@@ -310,9 +309,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val PARKS: Category = Category(
-            geocodingCanonicalName = "park",
-            sbsCanonicalName = "park",
+        val PARKS: Category = Category(
+            geocodingName = "park",
+            searchBoxName = "park",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_parks,
                 R.drawable.mapbox_search_sdk_ic_category_park
@@ -323,9 +322,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val MOVIE_THEATERS: Category = Category(
-            geocodingCanonicalName = "cinema",
-            sbsCanonicalName = "cinema",
+        val MOVIE_THEATERS: Category = Category(
+            geocodingName = "cinema",
+            searchBoxName = "cinema",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_movie_theaters,
                 R.drawable.mapbox_search_sdk_ic_category_cinema
@@ -336,9 +335,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val GYM_FITNESS: Category = Category(
-            geocodingCanonicalName = "fitness center",
-            sbsCanonicalName = "fitness_center",
+        val GYM_FITNESS: Category = Category(
+            geocodingName = "fitness center",
+            searchBoxName = "fitness_center",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_gym_fitness,
                 R.drawable.mapbox_search_sdk_ic_category_gym
@@ -349,9 +348,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val NIGHT_CLUBS: Category = Category(
-            geocodingCanonicalName = "nightclub",
-            sbsCanonicalName = "nightclub",
+        val NIGHT_CLUBS: Category = Category(
+            geocodingName = "nightclub",
+            searchBoxName = "nightclub",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_night_clubs,
                 R.drawable.mapbox_search_sdk_ic_category_night_club
@@ -362,9 +361,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val AUTO_REPAIR_MECHANIC: Category = Category(
-            geocodingCanonicalName = "auto repair",
-            sbsCanonicalName = "auto_repair",
+        val AUTO_REPAIR_MECHANIC: Category = Category(
+            geocodingName = "auto repair",
+            searchBoxName = "auto_repair",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_auto_repair_mechanic,
                 R.drawable.mapbox_search_sdk_ic_category_car_repair
@@ -375,9 +374,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val ATM: Category = Category(
-            geocodingCanonicalName = "atm",
-            sbsCanonicalName = "atm",
+        val ATM: Category = Category(
+            geocodingName = "atm",
+            searchBoxName = "atm",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_atm,
                 R.drawable.mapbox_search_sdk_ic_category_atm
@@ -388,9 +387,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val HOSPITAL: Category = Category(
-            geocodingCanonicalName = "hospital",
-            sbsCanonicalName = "hospital",
+        val HOSPITAL: Category = Category(
+            geocodingName = "hospital",
+            searchBoxName = "hospital",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_hospital,
                 R.drawable.mapbox_search_sdk_ic_category_hospital
@@ -401,9 +400,9 @@ internal class Category(
          * Predefined category constant.
          */
         @JvmField
-        public val EMERGENCY_ROOM: Category = Category(
-            geocodingCanonicalName = "emergency room",
-            sbsCanonicalName = "emergency_room",
+        val EMERGENCY_ROOM: Category = Category(
+            geocodingName = "emergency room",
+            searchBoxName = "emergency_room",
             presentation = Presentation(
                 R.string.mapbox_search_sdk_category_name_emergency_room,
                 R.drawable.mapbox_search_sdk_ic_category_emergency_room
@@ -439,13 +438,13 @@ internal class Category(
             EMERGENCY_ROOM,
         )
 
-        private val MAPPING = PREDEFINED_CATEGORY_VALUES.map { it.geocodingCanonicalName to it }.toMap()
-        private val SBS_MAPPING = PREDEFINED_CATEGORY_VALUES.map { it.sbsCanonicalName to it }.toMap()
+        private val MAPPING = PREDEFINED_CATEGORY_VALUES.associateBy { it.geocodingName }
+        private val SEARCH_BOX_MAPPING = PREDEFINED_CATEGORY_VALUES.associateBy { it.searchBoxName }
 
         /**
-         * Looks up for a [Category] by any of SBS or geocoding canonical names.
+         * Looks up for a [Category] by any of Search Box or geocoding canonical names.
          */
         @JvmStatic
-        public fun findByCanonicalName(name: String): Category? = MAPPING[name] ?: SBS_MAPPING[name]
+        fun findByCanonicalName(name: String): Category? = MAPPING[name] ?: SEARCH_BOX_MAPPING[name]
     }
 }
