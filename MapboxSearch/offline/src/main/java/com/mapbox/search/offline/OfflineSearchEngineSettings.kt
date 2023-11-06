001d3fbcb77caba334f75c7105704da74494a4e8
globaltrouble <anton.novikau@mapbox.com>
2023-11-06 15:24:03 +0100

    [demo] Change endpoint to production one

--- MapboxSearch/offline/src/main/java/com/mapbox/search/offline/OfflineSearchEngineSettings.kt
+++ MapboxSearch/offline/src/main/java/com/mapbox/search/offline/OfflineSearchEngineSettings.kt
@@ -150,9 +150,10 @@ public class OfflineSearchEngineSettings @JvmOverloads constructor(
 
     internal companion object {
 
-        val DEFAULT_ENDPOINT_URI: URI = URI.create("https://search-sdk-offline-staging.tilestream.net")
-        const val DEFAULT_DATASET = "experimental/poi-us-west-test-1"
-        const val DEFAULT_VERSION = "v2"
+        // TODO: change to POI
+        val DEFAULT_ENDPOINT_URI: URI = URI.create("https://api.mapbox.com")
+        const val DEFAULT_DATASET = "mbx-main"
+        const val DEFAULT_VERSION = ""
 
         private fun defaultTileStore() = TileStore.create()
     }
