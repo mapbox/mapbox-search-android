bffa323a4d9172174e69a48d5d714993db8d66b9
Mike Ringrose <mike.ringrose@gmail.com>
2024-03-26 09:16:34 -0400

    fix(sdk-common): removing newline from mapbox ids

--- MapboxSearch/sdk-common/src/main/java/com/mapbox/search/common/OsmIdUtils.kt
+++ MapboxSearch/sdk-common/src/main/java/com/mapbox/search/common/OsmIdUtils.kt
@@ -23,7 +23,7 @@ public object OsmIdUtils {
             val originalId = poiId / 10
             val mbxPoiId = "urn:mbxpoi-osm:$osmType$originalId"
             val bytes = mbxPoiId.toByteArray(Charsets.UTF_8)
-            return Base64.encodeToString(bytes, Base64.URL_SAFE)
+            return Base64.encodeToString(bytes, Base64.URL_SAFE or Base64.NO_WRAP)
         }
     }
 }
