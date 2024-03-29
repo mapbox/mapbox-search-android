e90bd37b836e55ff328b257d551462e4ec10d5d5
Eugene <146015512+eugenes78@users.noreply.github.com>
2024-03-29 09:52:27 -0700

    Filter out offline results with empty name field

--- MapboxSearch/offline/src/main/java/com/mapbox/search/offline/OfflineSearchCallbackAdapter.kt
+++ MapboxSearch/offline/src/main/java/com/mapbox/search/offline/OfflineSearchCallbackAdapter.kt
@@ -2,13 +2,19 @@ package com.mapbox.search.offline
 
 import com.mapbox.search.base.BaseResponseInfo
 import com.mapbox.search.base.BaseSearchCallback
+import com.mapbox.search.base.assertDebug
 import com.mapbox.search.base.result.BaseSearchResult
 
 internal class OfflineSearchCallbackAdapter(private val callback: OfflineSearchCallback) : BaseSearchCallback {
     override fun onResults(results: List<BaseSearchResult>, responseInfo: BaseResponseInfo) {
         callback.onResults(
-            results.map { OfflineSearchResult(it.rawSearchResult) },
-            OfflineResponseInfo(responseInfo.requestOptions.core.mapToOfflineSdkType())
+            results = results
+                .filter {
+                    assertDebug(it.name.isNotBlank()) { "Empty name" }
+                    it.name.isNotBlank()
+                }
+                .map { OfflineSearchResult(it.rawSearchResult) },
+            responseInfo = OfflineResponseInfo(responseInfo.requestOptions.core.mapToOfflineSdkType())
         )
     }
 
