876dd1b7d621f779e6cfdb3a606212a44b80826e
Eugene <146015512+eugenes78@users.noreply.github.com>
2024-04-01 12:56:03 -0700

    Remove assertDebug since customers can't do anything about this condition

--- MapboxSearch/offline/src/main/java/com/mapbox/search/offline/OfflineSearchCallbackAdapter.kt
+++ MapboxSearch/offline/src/main/java/com/mapbox/search/offline/OfflineSearchCallbackAdapter.kt
@@ -2,17 +2,13 @@ package com.mapbox.search.offline
 
 import com.mapbox.search.base.BaseResponseInfo
 import com.mapbox.search.base.BaseSearchCallback
-import com.mapbox.search.base.assertDebug
 import com.mapbox.search.base.result.BaseSearchResult
 
 internal class OfflineSearchCallbackAdapter(private val callback: OfflineSearchCallback) : BaseSearchCallback {
     override fun onResults(results: List<BaseSearchResult>, responseInfo: BaseResponseInfo) {
         callback.onResults(
             results = results
-                .filter {
-                    assertDebug(it.name.isNotBlank()) { "Empty name" }
-                    it.name.isNotBlank()
-                }
+                .filter { it.name.isNotBlank() }
                 .map { OfflineSearchResult(it.rawSearchResult) },
             responseInfo = OfflineResponseInfo(responseInfo.requestOptions.core.mapToOfflineSdkType())
         )
