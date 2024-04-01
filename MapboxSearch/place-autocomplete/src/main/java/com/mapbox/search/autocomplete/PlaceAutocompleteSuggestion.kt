3534f19a9fe53cea2bb98ab73a1c3cbf290ebb1d
Eugene <146015512+eugenes78@users.noreply.github.com>
2024-04-01 13:28:41 -0700

    Make coordinate optional for AddressAutofill/PlaceAutocomplete (#212)

--- MapboxSearch/place-autocomplete/src/main/java/com/mapbox/search/autocomplete/PlaceAutocompleteSuggestion.kt
+++ MapboxSearch/place-autocomplete/src/main/java/com/mapbox/search/autocomplete/PlaceAutocompleteSuggestion.kt
@@ -1,6 +1,7 @@
 package com.mapbox.search.autocomplete
 
 import android.os.Parcelable
+import com.mapbox.geojson.Point
 import com.mapbox.search.base.result.BaseSearchResult
 import com.mapbox.search.base.result.BaseSearchSuggestion
 import com.mapbox.search.base.utils.extension.safeCompareTo
@@ -22,6 +23,11 @@ public class PlaceAutocompleteSuggestion internal constructor(
      * Formatted address.
      */
     public val formattedAddress: String?,
+
+    /**
+     * Place geographic point.
+     */
+    public val coordinate: Point?,
 
     /**
      * List of points near [coordinate], that represents entries to associated building.
@@ -72,6 +78,7 @@ public class PlaceAutocompleteSuggestion internal constructor(
 
         if (name != other.name) return false
         if (formattedAddress != other.formattedAddress) return false
+        if (coordinate != other.coordinate) return false
         if (routablePoints != other.routablePoints) return false
         if (makiIcon != other.makiIcon) return false
         if (!distanceMeters.safeCompareTo(other.distanceMeters)) return false
@@ -88,6 +95,7 @@ public class PlaceAutocompleteSuggestion internal constructor(
     override fun hashCode(): Int {
         var result = name.hashCode()
         result = 31 * result + (formattedAddress?.hashCode() ?: 0)
+        result = 31 * result + (coordinate?.hashCode() ?: 0)
         result = 31 * result + (routablePoints?.hashCode() ?: 0)
         result = 31 * result + (makiIcon?.hashCode() ?: 0)
         result = 31 * result + (distanceMeters?.hashCode() ?: 0)
@@ -104,6 +112,7 @@ public class PlaceAutocompleteSuggestion internal constructor(
         return "PlaceAutocompleteSuggestion(" +
                 "name='$name', " +
                 "formattedAddress=$formattedAddress, " +
+                "coordinate=$coordinate, " +
                 "routablePoints=$routablePoints, " +
                 "makiIcon=$makiIcon, " +
                 "distanceMeters=$distanceMeters, " +
