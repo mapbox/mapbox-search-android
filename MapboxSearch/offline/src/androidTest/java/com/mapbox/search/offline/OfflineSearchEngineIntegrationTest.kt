2c965427b28740dd0993581e9e169e106e74185b
Mike Ringrose <mike.ringrose@gmail.com>
2024-05-08 17:07:36 -0400

    style: adding comment to variable

--- MapboxSearch/offline/src/androidTest/java/com/mapbox/search/offline/OfflineSearchEngineIntegrationTest.kt
+++ MapboxSearch/offline/src/androidTest/java/com/mapbox/search/offline/OfflineSearchEngineIntegrationTest.kt
@@ -659,6 +659,7 @@ internal class OfflineSearchEngineIntegrationTest {
             serverIndex = null,
         )
 
+        // assumes an accuracy of approximately 10 meters
         const val DOUBLE_COMPARISON_EPS = 0.0001
 
         fun Double.approximatelyEquals(other: Double) = abs(this - other) < DOUBLE_COMPARISON_EPS
