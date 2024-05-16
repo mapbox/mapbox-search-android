00a5487dc8c5489388c319d554fcef9a01237fe8
Eugene <146015512+eugenes78@users.noreply.github.com>
2024-05-16 12:03:53 -0700

    Improve samples by adding UI elements to "silent" tests

--- MapboxSearch/sample/src/main/java/com/mapbox/search/sample/api/HistoryDataProviderKotlinExample.kt
+++ MapboxSearch/sample/src/main/java/com/mapbox/search/sample/api/HistoryDataProviderKotlinExample.kt
@@ -1,36 +1,37 @@
 package com.mapbox.search.sample.api
 
-import android.os.Bundle
-import android.util.Log
-import androidx.appcompat.app.AppCompatActivity
 import com.mapbox.search.ServiceProvider
 import com.mapbox.search.common.AsyncOperationTask
 import com.mapbox.search.common.CompletionCallback
 import com.mapbox.search.record.HistoryRecord
+import com.mapbox.search.sample.R
 
-class HistoryDataProviderKotlinExample : AppCompatActivity() {
+class HistoryDataProviderKotlinExample : BaseKotlinExampleActivity() {
+
+    override val titleResId: Int = R.string.action_open_history_data_provider_kt_example
 
     private val historyDataProvider = ServiceProvider.INSTANCE.historyDataProvider()
 
-    private lateinit var task: AsyncOperationTask
+    private var task: AsyncOperationTask? = null
 
     private val callback: CompletionCallback<List<HistoryRecord>> = object : CompletionCallback<List<HistoryRecord>> {
         override fun onComplete(result: List<HistoryRecord>) {
-            Log.i("SearchApiExample", "History records: $result")
+            logI("SearchApiExample", "History records:", result)
+            onFinished()
         }
 
         override fun onError(e: Exception) {
-            Log.i("SearchApiExample", "Unable to retrieve history records", e)
+            logI("SearchApiExample", "Unable to retrieve history records", e)
+            onFinished()
         }
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+    override fun startExample() {
         task = historyDataProvider.getAll(callback)
     }
 
     override fun onDestroy() {
-        task.cancel()
+        task?.cancel()
         super.onDestroy()
     }
 }
