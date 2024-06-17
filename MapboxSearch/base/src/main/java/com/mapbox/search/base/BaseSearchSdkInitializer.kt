40fc2986d7dde9ae58bfe3b9d42942f97c679c1d
Mike Ringrose <mike.ringrose@gmail.com>
2024-06-17 13:05:34 -0400

    style: fixing linter issues

--- MapboxSearch/base/src/main/java/com/mapbox/search/base/BaseSearchSdkInitializer.kt
+++ MapboxSearch/base/src/main/java/com/mapbox/search/base/BaseSearchSdkInitializer.kt
@@ -1,14 +1,6 @@
 package com.mapbox.search.base
 
-import android.app.Application
-import android.content.Context
-import androidx.startup.Initializer
 import com.mapbox.common.BaseMapboxInitializer
-import com.mapbox.common.MapboxSDKCommonInitializer
-import com.mapbox.common.SdkInfoRegistryFactory
-import com.mapbox.common.SdkInformation
-import com.mapbox.common.core.module.CommonSingletonModuleProvider
-import com.mapbox.search.base.utils.UserAgentProvider
 
 class BaseSearchSdkInitializer : BaseMapboxInitializer<Unit>() {
     override val initializerClass = BaseSearchSdkInitializerImpl::class.java
