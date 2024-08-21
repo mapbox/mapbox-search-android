92167999c6e9e5d12795d635a9400827b254c051
Ahmed Abdelrazek <ahmed.abdelrazek@mapbox.com>
2024-08-20 22:54:45 -0700

    Switching to native v10 and dataset v11

--- MapboxSearch/sample/src/main/java/com/mapbox/search/sample/TileDownloadUiActivity.kt
+++ MapboxSearch/sample/src/main/java/com/mapbox/search/sample/TileDownloadUiActivity.kt
@@ -416,7 +416,7 @@ class TileDownloadUiActivity : AppCompatActivity() {
 
     private fun loadRegion(centerPoint: Point, geometry: Geometry, radiusKm: Int, downloadProgress: TextView) {
         // configure address tiles download
-        val descriptors = listOf(OfflineSearchEngine.createTilesetDescriptor("experimental-poi-cat-alias", "v9"))
+        val descriptors = listOf(OfflineSearchEngine.createTilesetDescriptor("experimental-poi-cat-alias", "v11"))
 
         val loadOptions = TileRegionLoadOptions
             .Builder()
